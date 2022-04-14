c2e2fa784014d5adf545cb15eaac8a8d72184b3d
agustinpfr1 <100004503+agustinpfr1@users.noreply.github.com>
2022-04-14 20:35:57 -0300

    changes

---
+++ entrega1/app.js
@@ -0,0 +1,9 @@
+let tabla = prompt('Ingresa un número')
+
+for (multiplicar=0; multiplicar<=10; multiplicar+=1) {
+  document.write(tabla + " x " + multiplicar + " = " + (multiplicar)*(tabla) + "</br>")
+
+}
+
+
+
